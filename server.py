2cab593a1790ffa14d53c55158fc17501dfd9ecf
atg-abhijay <atg.abhijay@gmail.com>
2019-01-15 20:24:04 -0500

    changed id implementation. implemented delete product. changed a few other things too

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from flask import Flask, jsonify, abort, make_response, request, url_for
 from tinydb import TinyDB, Query
 
@@ -5,17 +6,15 @@ db = TinyDB('db.json')
 products = db.table('products')
 
 app = Flask(__name__)
-num_products = 0
 
 '''
 DB functions
 '''
 
 def add_product(title, price, inventory_count):
-    global num_products
-    num_products += 1
-    products.insert({'title': title, 'price': price, 'inventory_count': inventory_count, 'uri': url_for('route_get_product', pid=num_products, _external=True)})
-    return num_products
+    product_id = str(uuid4())
+    products.insert({'title': title, 'price': price, 'inventory_count': inventory_count, 'uri': url_for('route_get_product', pid=product_id, _external=True)})
+    return product_id
 
 
 def return_all_products():
@@ -23,7 +22,8 @@ def return_all_products():
 
 
 def return_product(product_id):
-    return products.get(doc_id=product_id)
+    Product_query = Query()
+    return products.search(Product_query.uri == url_for('route_get_product', pid=product_id, _external=True))
 
 
 def find_products(search_title):
@@ -31,14 +31,25 @@ def find_products(search_title):
         return return_all_products()
 
     Product_query = Query()
-    matching_products = products.search(Product_query.title.test(find_func, search_title))
-    return matching_products
+    return products.search(Product_query.title.test(find_func, search_title))
 
 
-def find_func(prod_title, search_title):
-    if search_title in prod_title:
+def find_func(string, substring):
+    if substring in string:
         return True
+
     return False
+
+
+def delete_product(product_id):
+    # products.remove(doc_ids=[prod_id])
+    Product_query = Query()
+    prod_to_delete = products.search(Product_query.uri == url_for('route_get_product', pid=product_id, _external=True))
+    if not prod_to_delete:
+        return [False]
+
+    products.remove(doc_ids=[prod_to_delete[0].doc_id])
+    return [True, prod_to_delete[0]]
 
 
 '''
@@ -54,25 +65,25 @@ def route_get_products():
     return jsonify({'products': all_products})
 
 
-@app.route('/marketplace/api/product/<int:pid>', methods=['GET'])
+@app.route('/marketplace/api/product/<pid>', methods=['GET'])
 def route_get_product(pid):
     product = return_product(pid)
     if not product:
         abort(404)
 
-    return jsonify({'product': product})
+    return jsonify({'product': product[0]})
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'not found'}), 404)
+    return make_response(jsonify({'error': 'Product(s) not found'}), 404)
 
 
-@app.route('/marketplace/api/products', methods=['POST'])
+@app.route('/marketplace/api/add-product', methods=['POST'])
 def route_add_product():
     new_product_id = add_product(request.json['title'], request.json['price'], request.json['inventory_count'])
 
-    return jsonify({'product': return_product(new_product_id)}), 201
+    return jsonify({'product': return_product(new_product_id)[0]}), 201
 
 
 @app.route('/marketplace/api/find-products/<title>', methods=['GET'])
@@ -83,6 +94,14 @@ def route_find_products(title):
 
     return jsonify({'products': matching_products})
 
+
+@app.route('/marketplace/api/delete-product/<pid>', methods=['DELETE'])
+def route_delete_product(pid):
+    outcome = delete_product(pid)
+    if not outcome[0]:
+        return jsonify({'message': 'No such product exists'})
+
+    return jsonify({'removed_product': outcome[1], 'message': 'Product deleted successfully'})
 
 
 if __name__ == '__main__':
