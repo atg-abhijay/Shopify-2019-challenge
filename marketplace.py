42948da6ea8fdf12d2bb7b1d1c400111a853f901
atg-abhijay <atg.abhijay@gmail.com>
2019-01-19 19:42:13 -0500

    added documentation for helper functiosn

--- marketplace.py
+++ marketplace.py
@@ -504,10 +504,32 @@ def generate_order(uname):
 """Helper functions"""
 
 def generate_product_uri(product_id):
+    """
+    Generate a product URI based on its ID.
+
+    :param str product_id: ID of the product
+
+    :returns: A URI for the given product
+    :rtype: *str*
+
+    """
+
     return url_for('route_get_product', pid=product_id, _external=True)
 
 
 def find_func(string, substring):
+    """
+    Check if one string is present in another string.
+
+    :param str string: String to check in
+    :param str substring: Substring we are interested in
+
+    :returns:
+        - *True* if the substring is present in the string
+        - *False* if the substring is *not* present in the string
+
+    """
+
     if substring.lower() in string.lower():
         return True
 
